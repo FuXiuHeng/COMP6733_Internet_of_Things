8cf68d980272cc450aee608545a4de55f6a668c9
FuXiuHeng <hengfuxiu@gmail.com>
2018-08-28 14:56:07 +1000

    Minor bug fix

--- prac4/resources/res-motion.c
+++ prac4/resources/res-motion.c
@@ -14,7 +14,7 @@ PERIODIC_RESOURCE(res_motion,
          res_get_handler,
          NULL,
          NULL,
-         NULL;
+         NULL);
 
 static void
 res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
