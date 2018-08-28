0dfd19eacb6ff2102b08e04be052e58bade27999
FuXiuHeng <hengfuxiu@gmail.com>
2018-08-28 15:14:06 +1000

    Testing

--- prac4/resources/res-motion.c
+++ prac4/resources/res-motion.c
@@ -31,8 +31,14 @@ res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferr
 	printf("Hello Motion Sensor\r\n");
 
 	int x = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_X);
+	int y = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_Y);
+	int z = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_Z);
+	int a = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_ALL);
 
 	printf("X value is %d\r\n", x);
+	printf("Y value is %d\r\n", y);
+	printf("Z value is %d\r\n", z);
+	printf("A value is %d\r\n", a);
 	printf("\n\n\n\n");
 }
 
