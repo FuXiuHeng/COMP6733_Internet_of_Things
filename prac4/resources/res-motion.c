b9cbb6f62f4e95d943e91d2cfdf3fc265ca1456a
FuXiuHeng <hengfuxiu@gmail.com>
2018-08-28 15:02:33 +1000

    Minor bug fix

--- prac4/resources/res-motion.c
+++ prac4/resources/res-motion.c
@@ -19,6 +19,13 @@ RESOURCE(res_motion,
 static void
 res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
+	SENSOR_ACTIVATE(mpu_9250_sensor);
 	printf("Hello Motion Sensor\n");
+	printf("Hello Motion Sensor\n");
+	printf("Hello Motion Sensor\n");
+	printf("Hello Motion Sensor\n");
+	printf("Hello Motion Sensor\n");
+	printf(mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_X));
+	printf("\n\n\n\n");
 }
 
