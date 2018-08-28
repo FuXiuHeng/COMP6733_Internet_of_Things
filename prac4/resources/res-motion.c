95ada44aa0c6647a452580bc791220e5ac13cca8
FuXiuHeng <hengfuxiu@gmail.com>
2018-08-28 15:05:54 +1000

    Minor bug fix attempt

--- prac4/resources/res-motion.c
+++ prac4/resources/res-motion.c
@@ -23,7 +23,7 @@ RESOURCE(res_motion,
 static void
 res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
-	SENSOR_ACTIVATE(mpu_9250_sensor);
+	SENSORS_ACTIVATE(mpu_9250_sensor);
 	printf("Hello Motion Sensor\n");
 	printf("Hello Motion Sensor\n");
 	printf("Hello Motion Sensor\n");
