1d586f344fd980d8b87978255a4dd2033881bfb7
FuXiuHeng <hengfuxiu@gmail.com>
2018-08-28 14:54:34 +1000

    Not tested: Added motion sensor

---
+++ prac4/resources/res-motion.c
@@ -0,0 +1,24 @@
+
+#include "contiki.h"
+
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rest-engine.h"
+#include "board-peripherals.h"
+
+static void res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+PERIODIC_RESOURCE(res_motion,
+         "title=\"Motion\";rt=\"Temperature\";obs",
+         res_get_handler,
+         NULL,
+         NULL,
+         NULL;
+
+static void
+res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	printf("Hello Motion Sensor\n");
+}
+
